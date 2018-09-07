66d67daab879e6a4f6a99b08324aca73ba0ccb5f
Eric Zorn <ericzorn@Erics-MacBook-Pro-2018.local>
2018-09-07 16:19:53 -0400

    Added Props to NavBar

--- src/components/main-components/About.js
+++ src/components/main-components/About.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Navigation from '../sub-components/Navigation';
+import '../../assets/css/about.css';
 
 class About extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navigation/>
+        <Navigation sidebarMarginTop={-20} profileImgMarginLeft={2}/>
         <h1>About Page</h1>
       </React.Fragment>
     )
--- src/components/sub-components/Navigation.js
+++ src/components/sub-components/Navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ProfilePicture from '../../assets/img/profile-picture.jpg';
-import '../../assets/css/navigation.css';
 import { Link } from 'react-router-dom';
+import '../../assets/css/navigation.css';
 
 
 class Navigation extends Component {
@@ -21,13 +21,13 @@ class Navigation extends Component {
   render() {
     const { sidebarOpen } = this.state;  
     return (
-        <nav className={sidebarOpen ? "sidebar active" : "sidebar"}>
+        <nav className={sidebarOpen ? "sidebar active" : "sidebar"} style={{marginTop: this.props.sidebarMarginTop}}>
             <button className="toggle-menu-btn" onClick={this.toggleSidebar}>
                 &#9776;
             </button>
             <h1 className={'nav-title'}>Eric Zorn<i className="fas fa-code"></i></h1>
             <ul>
-                <img src={ProfilePicture} alt="profile-pic" className="profile-pic"/>
+                <img src={ProfilePicture} alt="profile-pic" className="profile-pic" style={{marginLeft: this.props.profileImgMarginLeft}}/>
                 <li><Link to={'/'} onClick={this.handleLinkToggle}>Home</Link></li>
                 <li><Link to={'/about'} onClick={this.handleLinkToggle}>About</Link></li>
                 <li><Link to={'/contact'} onClick={this.handleLinkToggle}>Contact Me</Link></li>
