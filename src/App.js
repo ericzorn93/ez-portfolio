28774132a6f04736f4783498a04f4e32d9b1d619
Eric Zorn <ericzorn@Erics-MacBook-Pro-2018.local>
2018-09-07 13:39:59 -0400

    Set Up React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+// Component Imports
+import Navigation from './components/sub-components/Navigation';
+import Home from './components/main-components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navigation />
+        <Switch>
+            <Route exact path={'/'} component={Home} />
+        </Switch>
       </div>
     );
   }
