8d2a0e6046f79fa039913432fe0d4334fe088386
Eric Zorn <ericzorn@Erics-MBP-2018.lan>
2018-09-07 23:27:39 -0400

    Navbar Complete

--- src/components/sub-components/Navigation.js
+++ src/components/sub-components/Navigation.js
@@ -2,24 +2,25 @@ import React, { Component } from 'react';
 import ProfilePicture from '../../assets/img/profile-picture.jpg';
 import { Link } from 'react-router-dom';
 import '../../assets/css/navigation.css';
+import { SocialIcon } from 'react-social-icons';
 
 
 class Navigation extends Component {
 
   state = {
       sidebarOpen: false
-  }; 
+  };
 
   toggleSidebar = () => {
     this.setState({ sidebarOpen: !this.state.sidebarOpen });
-  }; 
+  };
 
   handleLinkToggle = () => {
     this.setState({ sidebarOpen: !this.state.sidebarOpen });
   };
 
   render() {
-    const { sidebarOpen } = this.state;  
+    const { sidebarOpen } = this.state;
     return (
         <nav className={sidebarOpen ? "sidebar active" : "sidebar"} style={{marginTop: this.props.sidebarMarginTop, zIndex: 999}}>
             <button className="toggle-menu-btn" onClick={this.toggleSidebar}>
@@ -32,6 +33,16 @@ class Navigation extends Component {
                 <li><Link to={'/about'} onClick={this.handleLinkToggle}>About</Link></li>
                 <li><Link to={'/contact'} onClick={this.handleLinkToggle}>Contact Me</Link></li>
             </ul>
+            <div className="social-icons">
+                <SocialIcon url={"mailto:ericzorndesigns@gmail.com?subject=EZ%20Business%20Inquiry&body=Hey%20Eric,%20my%20name%20is%20,"} network={"email"} style={{width: 30, marginLeft: 5}}/>
+                <SocialIcon url={"https://www.linkedin.com/in/ericzorn/"} style={{width: 30, marginLeft: 5}}/>
+                <SocialIcon url={"https://github.com/ericzorn93"} style={{width: 30, marginLeft: 5}}/>
+                <SocialIcon url={"https://www.facebook.com/ezorn"} style={{width: 30, marginLeft: 5}}/>
+                <SocialIcon url={"https://www.instagram.com/ericzorndesigns/"} style={{width: 30, marginLeft: 5}}/>
+            </div>
+            <footer className={"nav-footer text-center"}>
+                <p style={{color: "white"}}>Made with <span style={{color: "#e25555",}}>&hearts;</span> by Eric Zorn</p>
+            </footer>
         </nav>
     )
   }
